61437eef5fa8609f57dfc6eb011bd7995150be21
code1023 <znu3173@gmail.com>
2021-06-14 13:14:24 +0900

    북마크 상세 기능

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
 from .models import Bookmark
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
 
 
 class BookmarkCreateView(CreateView):

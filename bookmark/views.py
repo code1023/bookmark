9c31db7a47a68e4de2bc3575c8661dedf468e5ce
code1023 <znu3173@gmail.com>
2021-06-14 12:59:51 +0900

    북마크 추가 기능

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 from .models import Bookmark
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark  # 어떤 모델의 입력을 받을 것인지 결정
+    fields = ['site_name', 'url']  # 어떤 필드들을 입력받을 것인지 설정
+    success_url = reverse_lazy('list')  # 글쓰기를 완료하고 이동할 페이지 설정
+    template_name_suffix = '_create'  # 사용할 템플릿의 접미사만 변경하는 설정값
 
 
 class BookmarkListView(ListView):
